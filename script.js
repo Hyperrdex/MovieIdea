a61ff07bc5141fd7435e22c56c7ae2e8aace6c96
Dev <devmdjbv1234@gmail.com>
2025-01-03 15:52:39 +0530

    Fixed key

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ key = "9b672e3b";
 // let url = ``;
 
 async function fetchMovie(movieName) {
-    const response = await fetch(`http://www.omdbapi.com/?t=${movieName}&apikey=${key}`);
+    const response = await fetch(`https://www.omdbapi.com/?t=${movieName}&apikey=${key}`);
     const data = await response.json();
     console.log(data);
     if(data.Response === "True"){
