2b66ab3a05f83f1a5a4c50763cf480e053db77d1
Dev <devmdjbv1234@gmail.com>
2025-01-03 15:20:45 +0530

    Completed the Movie Guide application

---
+++ script.js
@@ -0,0 +1,70 @@
+// console.log("working");
+
+const searchAreaValue= document.getElementById("movie-name");
+const searchButton = document.getElementById("search-btn");
+const result = document.getElementById("result");
+key = "9b672e3b";
+
+
+// let url = ``;
+
+async function fetchMovie(movieName) {
+    const response = await fetch(`http://www.omdbapi.com/?t=${movieName}&apikey=${key}`);
+    const data = await response.json();
+    console.log(data);
+    if(data.Response === "True"){
+        result.innerHTML= `
+            <div class="information">
+                <img src=${data.Poster} class="poster">
+                <div>
+                    <h2> ${data.Title}</h2>
+                    <div class="rating">
+                        <img src="star-icon.svg"> 
+                        <h4> ${data.imdbRating} </h4>
+                    </div>
+
+                    <div class="details">
+                        <span>${data.Rated}</span>
+                        <span>${data.Year}</span>
+                        <span>${data.Runtime}</span>
+                    </div>
+
+                    <div class="genre">
+                        <div> ${data.Genre.split(",").join
+                            ("</div><div>")}
+                        </div>
+                    </div>
+             </div>
+
+             <h3>Plot :</h3>
+             <p>${data.Plot}</p>
+             <h3>Cast :</h3>
+             <p>${data.Actors}</p> 
+             <h3>Director :</h3>
+             <p>${data.Director}</p>      
+             
+            `;
+    }
+
+    else{
+        result.innerHTML= `<h3 class="msg"> ${data.Error} </h3>`
+    }
+}
+
+function getMovie(){
+    
+    let movieName = searchAreaValue.value;
+
+    if(movieName.value===""){
+        result.innerHTML= `<h2 class="msg"> Please enter the movie name </h2> `
+        return
+    }
+    
+    else{
+        fetchMovie(movieName)
+    }
+
+}
+
+searchButton.addEventListener("click", getMovie);
+// window.addEventListener("load", getMovie);
